9e5a4b106d1d673a9ba48d50df21980161315b64
Unknown <serena.buxton@colorado.edu>
2017-11-27 22:05:11 -0700

    Fixed minor spelling issues

--- login.php
+++ login.php
@@ -15,10 +15,10 @@ if(isset ($_SESSION['password']))
 </head>
     <body>
         <h2>Login/registration</h2>
-        <p>New members, please complete the top form to register as a  user. Returning users, please complete the second form to log in.</p>
+        <p>New members, please complete the top form to register as a user. Returning users, please complete the second form to login.</p>
         <hr/>
         
-        <h3>New Memmber Registration</h3>
+        <h3>New Member Registration</h3>
         <form method="POST" action="register.php">
             
             <p>Enter your e-mail address:
@@ -27,13 +27,13 @@ if(isset ($_SESSION['password']))
                 <input type="text" name="password" value="<?=$password ?>"/></p>
             <p>Confirm your password:
             <input tpye="text" name="password2"/></p>
-            <p><em>(Passwords are case-sensitive and must be atleast 6 characters long)</em></p>
+            <p><em>(Passwords are case-sensitive and must be at least 6 characters long)</em></p>
             <input type="reset" name="reset" value="Reset Registration Form"/>
             <input type="submit" name="register" value="Register"/>
         </form>
     <hr/>
     
-    <h3>Returning Memmber Login</h3>
+    <h3>Returning Member Login</h3>
     <form method="post" action="VerifyLogin.php">
         <p>Enter your Email Address:
             <input type="email" name="email" value="<?=$email ?>" /></p>
