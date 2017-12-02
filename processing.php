1813d4e0c36901804802183c10adaa21a0ac0e10
Peter Borrell <pborrell@techguruhelpdesk.com>
2017-12-01 22:12:20 -0700

    Fixed the parsing debug... onto the index.php

--- processing.php
+++ processing.php
@@ -12,14 +12,47 @@ $card = $_REQUEST['card'];
 $emailCheck = FALSE;
 $firstNameCheck = FALSE;
 $lastNameCheck = FALSE;
-$donationsCheck = FALSE;
 
-if(!empty($card && is_numeric($card))){
-  $card = 1;
-}
-else{
-  $card = 0;
-}
+
+function validatecard($number)
+ {
+    global $type;
+
+    $cardtype = array(
+        "visa"       => "/^4[0-9]{12}(?:[0-9]{3})?$/",
+        "mastercard" => "/^5[1-5][0-9]{14}$/",
+        "amex"       => "/^3[47][0-9]{13}$/",
+        "discover"   => "/^6(?:011|5[0-9]{2})[0-9]{12}$/",
+    );
+
+    if (preg_match($cardtype['visa'],$number))
+    {
+  $type= "visa";
+        return TRUE;
+  
+    }
+    else if (preg_match($cardtype['mastercard'],$number))
+    {
+  $type= "mastercard";
+        return TRUE;
+    }
+    else if (preg_match($cardtype['amex'],$number))
+    {
+  $type= "amex";
+        return TRUE;
+  
+    }
+    else if (preg_match($cardtype['discover'],$number))
+    {
+  $type= "discover";
+        return TRUE;
+    }
+    else
+    {
+        return false;
+    } 
+ }
+
 
 $createCredit = "CREATE TABLE IF NOT EXISTS 'credit' (
   'key' INT(10) NOT NULL AUTO_INCREMENT,
@@ -53,22 +86,27 @@ $createName = "CREATE TABLE IF NOT EXISTS 'name' (
      PRIMARY KEY ('key')
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8";
 
-if (preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{3,}$/", $email)){
-  $emailCheck = TRUE
+if (preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{3,}$/", $email) == TRUE){
+  $emailCheck = TRUE;
 }
-if(preg_match(("/0-9]{16}/"), $card)){
+if(preg_match(("/0-9]{16}/"), $card) == TRUE){
   $cardCheck = TRUE;
 }
-if(preg_match("/[A-Za-z]}+/", $last_name)){
+if(preg_match("/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u", $last_name) == TRUE){
   $lastNameCheck = TRUE;
 }
-if(preg_match("/[A-Za-z]}+/", $first_name)){
+if(preg_match("/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u", $first_name) == TRUE){
   $firstNameCheck = TRUE;
 }
-if(is_numeric($donations) && $donations > 0){
-  $donationsCheck = TRUE;
+
+
+if(validatecard($card) == TRUE){
+  $card = 1;
 }
-if($emailCheck == TRUE && $cardCheck == TRUE && $lastNameCheck == TRUE && $firstNameCheck == TRUE)
+else{
+  $card = 0;
+}
+if($emailCheck == TRUE && $lastNameCheck == TRUE && $firstNameCheck == TRUE){
   $conn = new mysqli("127.0.0.1", "root", "pass", "GonePhishin");
 
 
@@ -117,23 +155,17 @@ if($emailCheck == TRUE && $cardCheck == TRUE && $lastNameCheck == TRUE && $first
 }
 
 else{
- $errors = ""
+ $errors = "";
  if($email == FALSE){
-  $errors = $errors . "email";
+  $errors = $errors . "email, ";
  }
  if($firstNameCheck == FALSE){
-  $errors = $error . "First Name";
+  $errors = $error . "First Name, ";
  }
  if($lastNameCheck == FALSE){
-  $errors = $errors . "Last Name";
+  $errors = $errors . "Last Name, ";
  }
- if($cardCheck == FALSE){
-  $errors = $errors . "Credit Card";
- }
- if($donationsCheck == FALSE){
-  $errors = $errors . "Donation Amount";
- }
-print "An error occured, make sure your '$errors' is valid.";
+ print "An error occured, make sure your $errors is valid.";
 include 'index.php';
 }
 ?>
